dddef38a2c3120e063706d9b7ce46aa071c1a930
changhao.ni <flyx5020@163.com>
2024-07-24 17:47:51 +0800

    add: menu management

--- server/model/system/sys_menu.go
+++ server/model/system/sys_menu.go
@@ -8,14 +8,13 @@ import (
 type SysMenu struct {
 	Id         uint64          `gorm:"primarykey;comment:菜单ID" json:"id"`
 	Name       string          `gorm:"type:varchar(32);comment:菜单名称" json:"name"`
-	Type       int8            `gorm:"type:tinyint(1);comment:菜单类型(1:目录,2:菜单)" json:"type"`
+	Type       int8            `gorm:"type:tinyint(1);comment:菜单类型(1:目录,2:菜单,3:按钮)" json:"type"`
 	Permission string          `gorm:"type:varchar(128);comment:权限标识" json:"permission"`
 	Path       string          `gorm:"type:varchar(128);comment:路由地址" json:"path"`
 	Component  string          `gorm:"type:varchar(256);comment:组件" json:"component"`
 	ParentId   uint64          `gorm:"default:0;comment:父菜单ID" json:"parentId"`
 	Status     int8            `gorm:"type:tinyint(1);default:1;comment:菜单状态(0:禁用,1:启用)" json:"status"`
 	Meta       json.RawMessage `gorm:"type:json;comment:菜单元数据" json:"meta"`
-	Buttons    json.RawMessage `gorm:"type:json;comment:按钮权限" json:"buttons"`
 	Children   []*SysMenu      `gorm:"-" json:"children,omitempty"`
 	common.BaseDO
 }
--- server/web/request/system/sys_menu_req.go
+++ server/web/request/system/sys_menu_req.go
@@ -4,13 +4,12 @@ import "encoding/json"
 
 type SysMenuAddReq struct {
 	Name       string          `json:"name" binding:"required"`    // 菜单名称
-	Type       int8            `json:"type" binding:"gte=1,lte=2"` // 菜单类型(1:目录,2:菜单)
+	Type       int8            `json:"type" binding:"gte=1,lte=3"` // 菜单类型(1:目录,2:菜单,3:按钮)
 	Permission string          `json:"permission"`                 // 权限标识
 	Path       string          `json:"path"`                       // 路由地址
 	Component  string          `json:"component"`                  // 组件
 	Status     int8            `json:"status"`                     // 菜单状态(0:禁用,1:启用)
 	Meta       json.RawMessage `json:"meta"`                       // 元数据
-	Buttons    json.RawMessage `json:"buttons"`                    // 按钮
 	ParentId   uint64          `json:"parentId"`                   // 父菜单ID
 }
 
